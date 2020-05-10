37b66e1be62fc8cc1fa007f4f35d14c80fa989e3
anasansari157 <anasansari157@gmail.com>
2020-05-10 15:08:58 +0530

    Mount polls route in app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const pollRouter = require("./routes/polls");
 
 //connect database
 mongoose.connect("mongodb://localhost/polling-booth", (err) => {
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/polls", pollRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
