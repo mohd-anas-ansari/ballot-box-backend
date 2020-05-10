be2cb0bdfe96d478c02ce9a08964cdf975b89cfd
anasansari157 <anasansari157@gmail.com>
2020-05-10 16:04:31 +0530

    Add controllers for Poll routes

---
+++ controllers/polls.js
@@ -0,0 +1,21 @@
+const Poll = require("../models/Poll");
+module.exports = {
+	//GET all polls
+	getPolls: async function (req, res) {
+		try {
+			let allPolls = await Poll.find({});
+			res.json(allPolls);
+		} catch (error) {
+			res.json(error.message);
+		}
+	},
+	//POST create new poll
+	createPoll: async function (req, res) {
+		try {
+			let poll = await Poll.create(req.body);
+			res.json({ success: true, poll });
+		} catch (error) {
+			res.send(error.message);
+		}
+	},
+};
