45443a323e80efc82968d412df615a83a5d2eec5
anasansari157 <anasansari157@gmail.com>
2020-05-10 14:30:44 +0530

    Add route to render the login page

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,13 @@ var router = express.Router();
 const userController = require("../controllers/users");
 
 /* GET users listing. */
-router.get("/", userController.loginForm);
+router.get("/", userController.loggedinUsers);
+//POST handle login route
 router.post("/login", userController.loginAuth);
+//GET Login form
+router.get("/login", userController.loginForm);
+//POST handle Register route
 router.post("/register", userController.registerUser);
 
+//export the router
 module.exports = router;
