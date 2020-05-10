c0ed259506a67676240318f1e5db00796d48a075
anasansari157 <anasansari157@gmail.com>
2020-05-10 13:29:09 +0530

    Add login and register, working without hashing the password

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const userController = require("../controllers/users");
 
 /* GET users listing. */
 router.get("/", userController.loginForm);
-router.get("/login", userController.loginAuth);
+router.post("/login", userController.loginAuth);
 router.post("/register", userController.registerUser);
 
 module.exports = router;
