4f00e330743fb45abf2c9f7382d56949b4ab686a
anasansari157 <anasansari157@gmail.com>
2020-05-11 23:21:15 +0530

    Use cotrollers to grant user authorisation, not working yet

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,13 @@ var router = express.Router();
 const userController = require("../controllers/users");
 
 /* GET users listing. */
-router.get("/", userController.loggedinUsers);
+router.get("/", userController.showloggedinUsers);
 //POST handle login route
 router.post("/login", userController.loginAuth);
 //GET Login form
 router.get("/login", userController.loginForm);
+//GET Register form
+router.get("/register", userController.registerForm);
 //POST handle Register route
 router.post("/register", userController.registerUser);
 
